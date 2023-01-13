55919fa5efd9b878dbfdc9aac365a1b840bac601
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-01-13 19:36:55 -0300

    Delete formInsert

--- form.php
+++ form.php
@@ -11,36 +11,23 @@
 <body>
     <h3>Entre com os dados do produto</h3>
     <form action="ProdutoController.php" method="post">
-
-    <p>Nome: <input type="text" name="productName" 
-        <?php if(isset($_GET["productName"])) ?> 
-            value=<?php $_GET["productName"] ?>>
-        </p>
-
+        <p>Nome: <input type="text" name="productName"  
+        <?php if (isset($_GET['productName']))?> value="<?php echo $_GET['productName']?>"></p>
         <p>Descrição: <input type="text" name="productDescription" 
-        <?php if(isset($_GET["productDescription"]))?> 
-        value=<?php $_GET["productDescription"] ?>>
-    </p>
+        <?php if (isset($_GET['productDescription']))?> value="<?php echo $_GET['productDescription']?>"></p>
+        <p>Categoria: <input type="text" name="category" 
+        <?php if (isset($_GET['category']))?> value="<?php echo $_GET['category']?>"></p>
+        <p>Quantidade: <input type="number" name="quantity"
+         <?php if (isset($_GET['quantity']))?> value="<?php echo $_GET['quantity']?>"></p>
+        <p>Valor: <input type="number" name="price" 
+        <?php if (isset($_GET['price']))?> value="<?php echo $_GET['price']?>"></p>
 
-            <p>Categoria: <input type="text" name="category"
-            <?php if(isset($_GET["category"]))?> 
-            value=<?php $_GET["category"] ?>>
-        </p>
-
-            <p>Quantidade: <input type="number" name="quantity"
-            <?php if(isset($_GET["quantity"]))?> 
-            value=<?php $_GET["quantity"] ?>>
-        </p>
-
-            <p>Valor: <input type="number" name="price"
-            <?php if(isset($_GET["price"]))?> 
-            value=<?php $_GET["price"] ?>>
-        </p>
-            <?php if (isset($_GET['id'])) { ?> 
-                <input type="hidden" name="id" >
+        <?php if (isset($_GET['id'])) { ?>
+            <p><input type="hidden" name="id" value="<?php echo $_GET['id']?>"></p>
             <p><input type="submit" name="operation" value="update"> </p>
-            <?php } else ?>
+        <?php } else { ?>
             <p><input type="submit" name="operation" value="insert"> </p>
+        <?php } ?>
         </p>
     </form>
 </body>
